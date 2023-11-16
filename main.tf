f5901ecc06104c22d6d511e7f53cae7c00a85799
Matthew James <vanitheria@gmail.com>
2023-11-16 23:02:56 +0000

    demo non-message.

---
+++ main.tf
@@ -0,0 +1,45 @@
+variable "zone" {
+  default = "us-central1-f"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "cis-91-devs"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network-1"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  zone         = var.zone
+  tags = ["dev", "web"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      # image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+output "ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+}
+
